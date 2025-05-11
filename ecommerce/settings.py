31b54abd42569abd802285a663bbcd8adb83f80e
Anowar-Alo <ahalo1164@gmail.com>
2025-05-11 20:21:49 +0600

    debbug functionality changed

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-development-key-change-in-production'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://127.0.0.1:8000']
